be0a0dddc997e92c39d8b47601a4a7dbb149cbca
ehddbs <ehddbs295@gmail.com>
2022-09-15 10:41:49 +0900

    socket

--- SocketLecture/SocketLecture/SocketLecture.cpp
+++ SocketLecture/SocketLecture/SocketLecture.cpp
@@ -37,7 +37,56 @@ int main();
 
 int process_client(client_type& new_client, std::vector<client_type>& client_array, std::thread& thread)
 {
+    std::string msg = "";
+    char tempmsg[DEFAULT_BUFLEN] = "";
+    while (true)
+    {
+        memset(tempmsg, 0, DEFAULT_BUFLEN);
 
+        if (new_client.socket != 0)
+        {
+            int iResult = recv(new_client.socket, tempmsg, DEFAULT_BUFLEN, 0);
+
+            if (iResult != SOCKET_ERROR)
+            {
+                if (strcmp("", tempmsg))
+                {
+                    msg = "client # " + std::to_string(new_client.id) + ": " + tempmsg;
+                }
+                std::cout << msg.c_str() << std::endl;
+
+                for (int i = 0; i < MAX_CLIENTS; i++)
+                {
+                    if (new_client.id != i)
+                    {
+                        iResult = send(client_array[i].socket, msg.c_str(), strlen(msg.c_str()), 0);
+                    }
+                }
+            }
+            else
+            {
+                msg = "client #" + std::to_string(new_client.id) + "disconnected";
+                std::cout << msg << std::endl;
+
+                closesocket(new_client.socket);
+                closesocket(client_array[new_client.id].socket);
+
+                client_array[new_client.id].socket = INVALID_SOCKET;
+
+                for (int i = 0; i < MAX_CLIENTS; i++)
+                {
+                    if (client_array[i].socket != INVALID_SOCKET)
+                    {
+                        iResult = send(client_array[i].socket, msg.c_str(), strlen(msg.c_str()), 0);
+                    }
+                }
+                break;
+            }
+        }
+        thread.detach();
+        return 0;
+    }
+    
 }
 
 //
@@ -139,49 +188,19 @@ int main(void)
 
 
     // No longer need server socket
-    /*
-    closesocket(ListenSocket);
+    
+    closesocket(server_socket);
 
-    // Receive until the peer shuts down the connection
-    do {
-
-        iResult = recv(ClientSocket, recvbuf, recvbuflen, 0); //데이터를 주고 받는
-        if (iResult > 0) {
-            printf("Bytes received: %d\n", iResult);
-
-            // Echo the buffer back to the sender
-            iSendResult = send(ClientSocket, recvbuf, iResult, 0);//데이터를 주고 받는
-            if (iSendResult == SOCKET_ERROR) {
-                printf("send failed with error: %d\n", WSAGetLastError());
-                closesocket(ClientSocket);
-                WSACleanup();
-                return 1;
-            }
-            printf("Bytes sent: %d\n", iSendResult);
-        }
-        else if (iResult == 0)
-            printf("Connection closing...\n");
-        else {
-            printf("recv failed with error: %d\n", WSAGetLastError());
-            closesocket(ClientSocket);
-            WSACleanup();
-            return 1;
-        }
-
-    } while (iResult > 0);
-
-    // shutdown the connection since we're done
-    iResult = shutdown(ClientSocket, SD_SEND);
-    if (iResult == SOCKET_ERROR) {
-        printf("shutdown failed with error: %d\n", WSAGetLastError());
-        closesocket(ClientSocket);
-        WSACleanup();
-        return 1;
+    for (int i = 0; i < MAX_CLIENTS; i++)
+    {
+        my_thread[i].detach();
+        closesocket(client[i].socket);
     }
 
-    // cleanup
-    closesocket(ClientSocket);
+
+
     WSACleanup();
-    */
+    std::cout << "end" << std::endl;
+
     return 0;
 }
